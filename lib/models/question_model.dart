da26b166b3028eb3d81a23d9bb469fa151257c7a
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-26 21:55:59 +0100

    quiz app

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -2,10 +2,15 @@ import 'package:equatable/equatable.dart';
 
 class Question extends Equatable{
   late final String category;
-late final String difficulties;
+  late final String difficulties;
+  final String question;
+  final String correctAnswer
+  final List<String> answers
+  
+  
+  
   @override
   // TODO: implement props
   List<Object> get props => throw UnimplementedError();
-
 
 }
