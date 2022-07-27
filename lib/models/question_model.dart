b31c70b292b278f0781b15651e11d674ea783de0
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-27 22:21:52 +0100

    quiz app

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -2,15 +2,41 @@ import 'package:equatable/equatable.dart';
 
 class Question extends Equatable{
   late final String category;
-  late final String difficulties;
+  late final String difficulty;
   final String question;
   final String correctAnswer
   final List<String> answers
   
+  const Question({
+    @required this.category
+    @required this.difficulty
+    @required this.question
+    @required this.correctAnswer
+    @required this.answers
+  })
   
-  
+
   @override
   // TODO: implement props
-  List<Object> get props => throw UnimplementedError();
+  List<Object> get props => [
+    category,
+    difficulty,
+    question,
+    correctAnswer,
+    answers
+  ]
 
+//...
+factory Question.fromMap(Map<String, dynamic> map){
+  if(map == null) return null
+  return Question(
+    category: map['category'] ?? '',
+    difficulty: map['difficulty'] ?? '',
+    question: map['question'] ?? '',
+    correctAnswer: map['correctAnswer'] ?? '',
+    answer: List<String>.from(map['incorrect_answers'] ?? [])
+    ..add(map['correct_answer'] ?? '')
+    ..shuffule(), 
+  )
+} 
 }
